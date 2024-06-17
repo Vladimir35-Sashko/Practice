3c1502b156b6d4f162cca8c8cf04d2a7e23f126a
Vladimir35-Sashko <Sashko852909@gmail.com>
2024-06-17 19:10:44 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <FirstTitle></FirstTitle>
+  </div>
+</template>
+
+<script>
+import FirstTitle from "./components/FirstTitle.vue";
+export default {
+  name: "App",
+  components: { FirstTitle },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
